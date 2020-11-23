672c127ed3786250896112074b8cc1584a04f0a3
heidariarash <arashheidari94@outlook.com>
2020-11-23 18:06:51 +0330

    adding new layer window

---
+++ scripts/new-layer-window.js
@@ -0,0 +1,10 @@
+const electron = require('electron');
+const {ipcRenderer} = electron;
+
+document.getElementById('done').addEventListener('click', () => {
+    ipcRenderer.send('add-new-layer', document.getElementById('layer-selector').value);
+});
+
+document.getElementById('cancle').addEventListener('click', () => {
+    ipcRenderer.send('close-small');
+});
