35277dcbeacf904c7f79dac5c8628b072e58fc99
heidariarash <arashheidari94@outlook.com>
2020-11-23 18:09:07 +0330

    updating the functionality of new layer button

--- scripts/main-window.js
+++ scripts/main-window.js
@@ -1,5 +1,7 @@
+const add_layer_to_window = require("../scripts/utils/new-layer.js");
 const electron = require('electron');
 const {ipcRenderer} = electron;
+
 
 document.getElementById('close-btn').addEventListener('click', () => {
     ipcRenderer.send('exit-app');
@@ -12,3 +14,11 @@ document.getElementById('min-btn').addEventListener('click', () => {
 document.getElementById('max-btn').addEventListener('click', () => {
     ipcRenderer.send('max-app');
 });
+
+document.getElementById('new-layer-button').addEventListener('click', () => {
+    add_new_layer_buttons({id: 'new-layer-button-parent-add'})
+});
+
+const add_new_layer_buttons = element => {
+    ipcRenderer.send('new-layer-request', element.id.slice(0,-4));
+}
